04f6d5c35ef4cea34ce90d8a67ccdeb63fddd925
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-04-15 23:33:51 +0530

    updated render request url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/loginuser', {
+      const response = await axios.post('https://tastify.onrender.com/api/loginuser', {
         email,
         password
       });
